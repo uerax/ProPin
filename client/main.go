ad9bd0979539d64b191e59b6d607230c4e7d5799
wesker <uerax@live.com>
2023-11-06 00:16:04 +0800

    feat: add flag

--- client/main.go
+++ client/main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"flag"
+	"time"
 )
 
 var (
 	server string
+	ipme   = "https://api.ipify.org/?format=text"
 )
 
 func main() {
-	flag.StringVar(&server, "s", "", "主机域名/IP")
+	flag.StringVar(&server, "s", "", "探针ProPin中心域名/IP")
 	flag.Parse()
+	if server == "" {
+		panic("没有提供探针ProPin中心访问链接")
+	}
+ 	t := time.NewTicker(time.Minute)
+	for range t.C {
+		// 直接在服务端拿请求ip即可
+		// res, err := http.Get(ipme)
+		// if err == nil && res.StatusCode == http.StatusOK {
+		// 	defer res.Body.Close()
+		// 	ip, err := io.ReadAll(res.Body)
+		// 	if err == nil {
+		// 		fmt.Println(string(ip))
+		// 	}
+		// }
+		
+	}
+
 }
