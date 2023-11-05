04569a6e9fa7309c800d4ef3a4f715489e705c17
rotlands <uerax@live.com>
2023-11-06 00:55:59 +0800

    feat: client base done

--- client/main.go
+++ client/main.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"net/http"
 	"time"
 )
 
 var (
-	server string
+	serverApi string
 	ipme   = "https://api.ipify.org/?format=text"
 )
 
 func main() {
-	flag.StringVar(&server, "s", "", "探针ProPin中心域名/IP")
+	flag.StringVar(&serverApi, "s", "", "探针ProPin中心域名/IP")
 	flag.Parse()
-	if server == "" {
+	if serverApi == "" {
 		panic("没有提供探针ProPin中心访问链接")
 	}
- 	t := time.NewTicker(time.Minute)
+	t := time.NewTicker(time.Minute)
+
+	do := func() {
+		status := &Status{}
+		status.Coll()
+		status.Report()
+	}
+
 	for range t.C {
 		// 直接在服务端拿请求ip即可
 		// res, err := http.Get(ipme)
@@ -27,7 +38,31 @@ func main() {
 		// 		fmt.Println(string(ip))
 		// 	}
 		// }
-		
+		go do()
 	}
 
+}
+
+type Status struct {
+	
+}
+
+func (t *Status) Coll() {
+	
+	
+}
+
+func (t *Status) Report() {
+	data, err := json.Marshal(t)
+		if err != nil {
+			fmt.Println("JSON编码失败:", err)
+			return
+		}
+
+		resp, err := http.Post(serverApi, "application/json", bytes.NewBuffer(data))
+		if err != nil {
+			fmt.Println("POST请求失败:", err)
+			return
+		}
+		defer resp.Body.Close()
 }
